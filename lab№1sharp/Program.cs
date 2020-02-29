806f3e53619405ebd150a614967be4bfdbce74da
lesti <yaseloklass@gmail.com>
2020-02-29 16:25:12 +0300

    Добавьте файлы проекта.

---
+++ lab№1sharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_1sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, Z;
+            Console.WriteLine("Введите целое число a: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите целое число b: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            Z = Math.Pow((a - b), 2);
+            Console.WriteLine("Z = " + Z);
+        }
+    }
+}
